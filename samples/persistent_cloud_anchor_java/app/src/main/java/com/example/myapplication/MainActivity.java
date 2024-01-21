00c1446f5ba8dbca418e6ac523a6082dd54f2c82
Eerohne <Loufi0002@gmail.com>
2024-01-21 08:59:59 -0500

    Save Systen

--- samples/persistent_cloud_anchor_java/app/src/main/java/com/example/myapplication/MainActivity.java
+++ samples/persistent_cloud_anchor_java/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,18 +15,29 @@ import com.example.myapplication.structures.runs.Run;
 import com.example.myapplication.structures.runs.SimpleRun;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.ar.core.examples.java.persistentcloudanchor.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView runCardPanel;
     ArrayList<Run> runArrayList;
 
+    public static final String PREF = "PREF";
+    private static final String KEY = "KEY_DATA";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //loadSaveFile();
 
         runCardPanel = findViewById(R.id.game_cards_panel);
 
@@ -33,15 +45,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Test the pane
         runArrayList = new ArrayList<>();
-        runArrayList.add(new SimpleRun("JMSB Hunt", "Roam Concordia and discover its amazing campuses", 0));
-        runArrayList.add(new SimpleRun("Hall Hunt", "Roam Concordia and discover its amazing campuses", 0));
-        runArrayList.add(new SimpleRun("McGill Hunt", "Roam McGill and discover its amazing campuses", 0));
-        runArrayList.add(new SimpleRun("UdeM Hunt", "Roam UdeM and discover its amazing campuses", 0));
-
+        loadSaveFile();
+        runArrayList.add(new SimpleRun("JMSB Hunt" + new Random().nextInt(), "Roam Concordia and discover its amazing campuses", 0));
+        //runArrayList.add(new SimpleRun("Hall Hunt", "Roam Concordia and discover its amazing campuses", 0));
+        //runArrayList.add(new SimpleRun("McGill Hunt", "Roam McGill and discover its amazing campuses", 0));
+        //runArrayList.add(new SimpleRun("UdeM Hunt", "Roam UdeM and discover its amazing campuses", 0));
         RunAdapter runAdapter = new RunAdapter(this, runArrayList);
 
         runCardPanel.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         runCardPanel.setAdapter(runAdapter);
+
+        storeSaveFile();
     }
 
     private void initializeSearchFunction(){
@@ -72,5 +86,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {}
         });
+    }
+
+    public void storeSaveFile()
+    {
+        String convertedData = new Gson().toJson(runArrayList); // converted to string.
+
+        SharedPreferences userDetails = this.getSharedPreferences("runDetails", MODE_PRIVATE);
+        SharedPreferences.Editor edit = userDetails.edit();
+
+        edit.putString("runList", convertedData);
+        edit.apply();
+    }
+
+    public void loadSaveFile()
+    {
+        Context context = this;
+        SharedPreferences sharedPref = context.getSharedPreferences("runDetails", Context.MODE_PRIVATE);
+        String GsonSaveString = sharedPref.getString("runList", "");
+
+        Type listType = new TypeToken<List<Run>>() {}.getType();
+        ArrayList<Run> saveData = new Gson().fromJson(GsonSaveString,  listType);
+        if(saveData != null)
+            runArrayList = saveData;
+        else
+            runArrayList = new ArrayList<>();
     }
 }
